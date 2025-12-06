7cb534cf2aabfb06b0921ba6ecb47bf266d2c9f8
Jvctori <137020639+Jvctori@users.noreply.github.com>
2025-12-05 21:03:18 -0300

    .

--- main.go
+++ main.go
@@ -21,6 +21,10 @@ func main() {
 		// perceba que a func PhoneNumber() começa com UPPERCASE
 		fmt.Println("Random phone number:", randomdata.PhoneNumber())
 		fmt.Println("Random Address:", randomdata.Address())
+		fmt.Println("Random phone number:", randomdata.PhoneNumber())
+		fmt.Println("Random phone number:", randomdata.PhoneNumber())
+
+
 		ops.PresentOpt()
 		ops.UserChoices()
 
